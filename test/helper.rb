5deb8a837c4205fa6b72af9d7243f7594ab4ebb1
Ryan Tomayko <rtomayko@gmail.com>
2012-06-21 05:45:12 -0700

    Get main Ernicorn module test working

--- test/helper.rb
+++ test/helper.rb
@@ -1,17 +1,4 @@
-require 'rubygems'
 require 'test/unit'
 require 'shoulda'
-
-ERNIE_ROOT = File.expand_path(File.join(File.dirname(__FILE__), *%w[..]))
-
-$:.unshift(File.join(ERNIE_ROOT, 'lib'))
-
-require 'ernie'
-Ernie.auto_start = false
-
-begin
-  require 'bertrpc'
-rescue LoadError
-  puts "You need bertrpc gem installed to run tests."
-  exit!(1)
-end
+require 'ernicorn'
+Ernicorn.auto_start = false
--- test/test_ernicorn.rb
+++ test/test_ernicorn.rb
@@ -1,6 +1,6 @@
-require File.dirname(__FILE__) + '/helper'
+require File.expand_path('../helper', __FILE__)
 
-class ErnieTest < Test::Unit::TestCase
+class ErnicornTest < Test::Unit::TestCase
   module TestExposingModule
     def foo
     end
@@ -19,24 +19,24 @@ class ErnieTest < Test::Unit::TestCase
   end
 
   context "expose" do
-    setup { Ernie.expose :expo, TestExposingModule }
-    teardown { Ernie.mods.clear }
+    setup { Ernicorn.expose :expo, TestExposingModule }
+    teardown { Ernicorn.mods.clear }
 
     should "add all public methods from the module" do
-      assert_not_nil Ernie.mods[:expo].funs[:foo]
-      assert_not_nil Ernie.mods[:expo].funs[:bar]
+      assert_not_nil Ernicorn.mods[:expo].funs[:foo]
+      assert_not_nil Ernicorn.mods[:expo].funs[:bar]
     end
 
     should "not expose protected methods" do
-      assert_nil Ernie.mods[:expo].funs[:baz]
+      assert_nil Ernicorn.mods[:expo].funs[:baz]
     end
 
     should "not expose private methods" do
-      assert_nil Ernie.mods[:expo].funs[:bling]
+      assert_nil Ernicorn.mods[:expo].funs[:bling]
     end
 
     should "dispatch to module methods properly" do
-      res = Ernie.dispatch(:expo, :bar, ['a', :b, { :fiz => 42 }])
+      res = Ernicorn.dispatch(:expo, :bar, ['a', :b, { :fiz => 42 }])
       assert_equal ['a', :b, { :fiz => 42 }], res
     end
   end
@@ -55,11 +55,11 @@ class ErnieTest < Test::Unit::TestCase
   end
 
   context "logging" do
-    setup { Ernie.expose :logging, TestLoggingModule; TestLoggingModule.logs.clear  }
-    teardown { Ernie.mods.clear }
+    setup { Ernicorn.expose :logging, TestLoggingModule; TestLoggingModule.logs.clear  }
+    teardown { Ernicorn.mods.clear }
 
     should "call logger method with timing" do
-      ret = Ernie.dispatch(:logging, :sleepy, 0.1)
+      ret = Ernicorn.dispatch(:logging, :sleepy, 0.1)
       assert_equal 0.1, ret
 
       assert_equal 1, TestLoggingModule.logs.size
